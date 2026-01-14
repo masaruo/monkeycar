ea6f814db1fcdfb777745b306a6567715516d889
masaru ogawa <masaruo@gmail.com>
2026-01-14 12:24:05 +0900

    pydantic added at shared

---
+++ shared/src/shared/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field, FilePath, time
+import time
+
+
+class DriveData(BaseModel):
+    """ラズパイで収集される1ステップ分の走行データ"""
+    # timestamp: 
+    image_name: str = Field(..., description="path to a corresponding image file")
+    steering: float = Field(..., ge=-1.0, le=1.0)
+    throttle: float = Field(..., ge=-1.0, le=1.0)
+
+# class InferenceResult(BaseModel):
+#     """FTLiteモデルからの出力データ"""
